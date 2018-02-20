89849850559c8be751dafb2a41bc67a73f7cbbf8
Dave Cohen <dcohen@zagster.com>
2018-02-20 14:01:49 -0500

    Fix

--- src/commands/messagesCommand.js
+++ src/commands/messagesCommand.js
@@ -1,8 +1,9 @@
 module.exports = {
   run: (message, userId, commandContent) => {
     if (commandContent) {
-      const to = commandContent.substr(0, commandString.indexOf(' ')).toLowerCase();
-      const text = commandContent.substr(commandString.indexOf(' ') + 1);
+      const to = commandContent.substr(0, commandContent.indexOf(' ')).toLowerCase();
+      const text = commandContent.substr(commandContent.indexOf(' ') + 1);
+      console.log(to);
       message.channel.send(`Message to ${to} reads ${text}.`);
     } else {
       message.channel.send('Message for you sir!');

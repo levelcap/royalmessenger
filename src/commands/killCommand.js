bc06131fe853ffed5a9a85ad5970abb7205ef7ff
Dave Cohen <dcohen@zagster.com>
2018-02-20 13:06:22 -0500

    Spelling is hard

--- src/commands/killCommand.js
+++ src/commands/killCommand.js
@@ -60,12 +60,12 @@ const getKillRank = (killCount) => {
 module.exports = {
   run: (message, userId, username, commandContent) => {
     if (commandContent) {
-      if (commmandContent.toLowerCase().trim() === 'fatlass') {
+      if (commandContent.toLowerCase().trim() === 'fatlass') {
         message.channel.send('"I\'m afraid I cannot do that, some things are too beautiful to damage on a whim," the messenger declares while shedding a single tear.');
         return;
       }
 
-      if (commmandContent.toLowerCase().trim() === 'kaden') {
+      if (commandContent.toLowerCase().trim() === 'kaden') {
         message.channel.send('"Creator Override Enagaged. Terminating Everyone", the messenger\'s eyes turn red and his skin sloughs off to reveal a skeleton of gleaming chrome beneath. He strangles everyone except Kaden to death. Suck it.');
         return;
       }

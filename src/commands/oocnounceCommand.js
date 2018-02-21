7216b31f899b21bb6479d189a944559b35bd4f83
Dave Cohen <dcohen@zagster.com>
2018-02-21 13:58:30 -0500

    Fix

--- src/commands/oocnounceCommand.js
+++ src/commands/oocnounceCommand.js
@@ -1,7 +1,7 @@
 const { getTitle } = require('../utils');
 
 module.exports = {
-  run: (message, userId) => {
+  run: (message, user) => {
     const title = getTitle(true, user._id);
     if (title) {
       message.channel.send(title);
